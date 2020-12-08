098067bc3e1a66ba2d6a79af883b9e97005374ad
binoy638 <binoy638@gmail.com>
2020-12-08 23:05:33 +0530

    Faqsection

---
+++ src/components/FaqSection.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+function FaqSection() {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="questions">
+        <h4>How DO i Start</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti,
+            officiis?
+          </p>
+        </div>
+      </div>
+      <div className="questions">
+        <h4>How DO i Start</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti,
+            officiis?
+          </p>
+        </div>
+      </div>
+      <div className="questions">
+        <h4>How DO i Start</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti,
+            officiis?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FaqSection;
--- src/pages/Aboutus.js
+++ src/pages/Aboutus.js
@@ -1,12 +1,14 @@
 import React from "react";
 import AboutSection from "../components/AboutSection";
 import ServivesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 
 function Aboutus() {
   return (
     <>
       <AboutSection />
       <ServivesSection />
+      <FaqSection />
     </>
   );
 }
