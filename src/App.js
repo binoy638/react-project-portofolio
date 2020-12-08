416f0a27eab5306bdc9c52a8ddc81fcf8adba01a
binoy638 <binoy638@gmail.com>
2020-12-08 23:35:16 +0530

    Styled components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import GlobalStyle from "./components/GlobalStyle";
 import Aboutus from "./pages/Aboutus";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <Aboutus />
     </div>
   );
